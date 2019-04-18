14f43c851ca60f6547c5ffd4a17c7815b3e1fb54
Johannes Link <business@johanneslink.net>
2019-04-18 17:03:42 +0200

    Added EuroConverterCollaborationProperties

--- src/test/java/net/johanneslink/eurocalc/pbt/EuroConverterProperties.java
+++ src/test/java/net/johanneslink/eurocalc/pbt/EuroConverterProperties.java
@@ -8,6 +8,8 @@ import org.mockito.*;
 import net.jqwik.api.*;
 import net.jqwik.api.constraints.*;
 
+import static net.johanneslink.eurocalc.RateProvider.*;
+
 @Group
 @Label("EuroConverter")
 class EuroConverterProperties {
@@ -17,7 +19,7 @@ class EuroConverterProperties {
 		@Property
 		void rates_are_used_to_calculate_euro_amounts(
 				@ForAll("nonEuroCurrencies") String nonEuroCurrency,
-				@ForAll @DoubleRange(min = RateProvider.MINIMUM_RATE, max = RateProvider.MAXIMUM_RATE) double exchangeRate,
+				@ForAll @DoubleRange(min = MINIMUM_RATE, max = MAXIMUM_RATE) double exchangeRate,
 				@ForAll @DoubleRange(min = 0.01, max = 1_000_000.0) double amount
 		) throws RateNotAvailable {
 			RateProvider provider = Mockito.mock(RateProvider.class);
@@ -44,6 +46,14 @@ class EuroConverterProperties {
 		return Arbitraries.of("USD", "CHF", "CAD");
 	}
 
+	@Provide
+	Arbitrary<String> illegalCurrencies() {
+		return Arbitraries.strings().alpha()
+						  .ofMaxLength(10)
+						  .map(String::toUpperCase)
+						  .filter(currency -> currency.length() != 3);
+	}
+
 	@Group
 	class Collaboration_Tests {
 
@@ -55,24 +65,50 @@ class EuroConverterProperties {
 			Mockito.when(provider.rate(nonEuroCurrency, "EUR")).thenReturn(0.5);
 
 			double euroAmount = new EuroConverter(provider).convert(10.0, nonEuroCurrency);
+			Assertions.assertThat(euroAmount).isGreaterThan(0.0);
+
 			Mockito.verify(provider).rate(nonEuroCurrency, "EUR");
 		}
 
-		@Example
-		void illegal_currencies_are_not_handed_to_rate_provider() throws RateNotAvailable {
+		@Property
+		void illegal_currencies_are_not_handed_to_rate_provider(
+				@ForAll("illegalCurrencies") String illegalCurrency
+		) throws RateNotAvailable {
 			RateProvider provider = Mockito.mock(RateProvider.class);
 
-			Assertions.assertThatThrownBy(() -> new EuroConverter(provider).convert(8.0, "ab"))
+			Assertions.assertThatThrownBy(() -> new EuroConverter(provider).convert(8.0, illegalCurrency))
 					  .isInstanceOf(IllegalArgumentException.class);
 
 			Mockito.verify(provider, Mockito.never()).rate(Mockito.anyString(), Mockito.anyString());
 		}
 
+		@Property
+		void can_handle_any_exchange_rate(
+				@ForAll @DoubleRange(min = MINIMUM_RATE, max = MAXIMUM_RATE) double exchangeRate,
+				@ForAll @DoubleRange(min = 0.01, max = 1_000_000.0) double amount
+		) {
+			String collector = exchangeRate == MINIMUM_RATE ? "min"
+									   : exchangeRate == MAXIMUM_RATE ? "max" : "other";
+			Statistics.collect(collector);
+			RateProvider provider = (fromCurrency, toCurrency) -> exchangeRate;
+			double euroAmount = new EuroConverter(provider).convert(amount, "USD");
+			Assertions.assertThat(euroAmount).isGreaterThan(0.0);
+		}
+
 		@Example
-		void can_handle_positive_exchange_rate() {
-			RateProvider provider = (fromCurrency, toCurrency) -> 0.8;
+		@Disabled("Not needed any more")
+		void can_handle_maximum_rate() {
+			RateProvider provider = (fromCurrency, toCurrency) -> MAXIMUM_RATE;
 			double euroAmount = new EuroConverter(provider).convert(8.0, "USD");
-			Assertions.assertThat(euroAmount).isCloseTo(6.4, Offset.offset(0.01));
+			Assertions.assertThat(euroAmount).isGreaterThanOrEqualTo(0.01);
+		}
+
+		@Example
+		@Disabled("Not needed any more")
+		void can_handle_minimum_rate() {
+			RateProvider provider = (fromCurrency, toCurrency) -> MINIMUM_RATE;
+			double euroAmount = new EuroConverter(provider).convert(10.0, "USD");
+			Assertions.assertThat(euroAmount).isGreaterThanOrEqualTo(0.01);
 		}
 
 		@Example
@@ -87,20 +123,6 @@ class EuroConverterProperties {
 			RateProvider provider = (fromCurrency, toCurrency) -> { throw new IllegalArgumentException("XYZ"); };
 			Assertions.assertThatThrownBy(() -> new EuroConverter(provider).convert(8.0, "XYZ"))
 					  .isInstanceOf(IllegalArgumentException.class);
-		}
-
-		@Example
-		void can_handle_maximum_rate() {
-			RateProvider provider = (fromCurrency, toCurrency) -> RateProvider.MAXIMUM_RATE;
-			double euroAmount = new EuroConverter(provider).convert(8.0, "USD");
-			Assertions.assertThat(euroAmount).isGreaterThanOrEqualTo(0.01);
-		}
-
-		@Example
-		void can_handle_minimum_rate() {
-			RateProvider provider = (fromCurrency, toCurrency) -> RateProvider.MINIMUM_RATE;
-			double euroAmount = new EuroConverter(provider).convert(10.0, "USD");
-			Assertions.assertThat(euroAmount).isGreaterThanOrEqualTo(0.01);
 		}
 
 	}
